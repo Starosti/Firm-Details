a480d8b01d718613cad80688ec81de6203434bbb
Starosti <syncingfx@gmail.com>
2019-05-31 13:41:11 +0300

    Added dots between numbers,fixed some typos,removed unused libraries and unused variables,made code a bit more readible

--- FirmCalculator/Firm.cs
+++ FirmCalculator/Firm.cs
@@ -4,20 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FirmCalculator
+namespace FirmCalculatorJson
 {
     public class Firm
     {
-        public int id { get; set; }
         public string name { get; set; }
         public long balance { get; set; }
         public int size { get; set; }
         public int execs { get; set; }
-        public int assocs { get; set; }
         public int coo { get; set; }
         public int cfo { get; set; }
+        public int assocs { get; set; }
+        public int board { get; set; }
         public int tax { get; set; }
         public int rank { get; set; }
-        public int last_payout { get; set; }
+
+        public Firm()
+        {
+            board = coo + cfo + 1;
+        }
     }
 }
--- FirmCalculator/Program.cs
+++ FirmCalculator/Program.cs
@@ -1,46 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
 using System.Net;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-
 using Newtonsoft.Json;
 using System.IO;
 
-namespace FirmCalculator
+namespace FirmCalculatorJson
 {
     class Program
     {
-        public static string ceoName = "ceo";
-        public static string cfoName = "cfo";
-        public static string cooName = "coo";
-        public static string execName = "exec";
-        public static string assocName = "assoc";
-
         static void Color(ConsoleColor _color)
         {
             Console.ForegroundColor = _color;
         }
+
+        private static void Between()
+        {
+            Color(ConsoleColor.Gray);
+            Console.WriteLine("");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("");
+        }
         private static bool CheckInt(string firmNum)
         {
-            if(int.TryParse(firmNum, out int a))
+            if(int.TryParse(firmNum,out int a) )
             {
-                return true;
-            }
-            else
-            {
+                if (Convert.ToInt32(firmNum) > 0) return true;
                 return false;
             }
+            return false;
         }
         static void Startup()
         {
             Color(ConsoleColor.DarkGray);
 
             Console.WriteLine(@"MemeEconomy Firm Payout App by /u/Starosti_
-Last Firm Distrubition Update Date:13.03.2019
+Last Firm Distribution Update Date:13.03.2019
 
 10% of the firm's money is taxed by MemeEconomy
 
@@ -65,57 +58,48 @@ Last Firm Distrubition Update Date:13.03.2019
                 Console.WriteLine("This firm doesn't exist!");
                 return;
             }
-            long floorMemPay = 0;
-            long assoPay = 0;
-            long execPay = 0;
+            long _floorMemPay = 0;
+            long _assoPay = 0;
+            long _execPay = 0;
             //calculations
+            string comma = "#,###";
             double tax = Convert.ToInt64(firmBal * 0.1);
             long payout = Convert.ToInt64((firmBal - tax) * 0.5);
-            long boardPay = Convert.ToInt64((payout * 0.3) / board);
-            if (!(exec == 0)) execPay = Convert.ToInt64((payout * 0.28) / exec);        
-            if (!(asso == 0)) assoPay = Convert.ToInt64((payout * 0.21) / asso);
-            if (exec + asso + board != totalM) floorMemPay = Convert.ToInt64((payout * 0.21) / floor);
+            long _boardPay = Convert.ToInt64((payout * 0.3) / board);
+            if (exec != 0) _execPay = Convert.ToInt64((payout * 0.28) / exec);        
+            if (asso != 0) _assoPay = Convert.ToInt64((payout * 0.21) / asso);
+            if (exec + asso + board != totalM) _floorMemPay = Convert.ToInt64((payout * 0.21) / floor);
+            string boardPay = _boardPay.ToString(comma);
+            string execPay = _execPay.ToString(comma);
+            string assoPay = _assoPay.ToString(comma);
+            string floorMemPay = _floorMemPay.ToString(comma);
+            string _payout = payout.ToString(comma);
             int total = board + exec + asso + floor;
             //output
-            Color(ConsoleColor.Gray);
-            Console.WriteLine("");
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine("");
+            Between();
             Color(ConsoleColor.Magenta);
             Console.WriteLine("Firm Name:" + firmName);
             Console.WriteLine("Firm Level:" + rank);
             Console.WriteLine("Firm Tax:"+taxP+"%");
-            Console.WriteLine("Firm Balance:" + firmBal);
-            Color(ConsoleColor.Gray);
-            Console.WriteLine("");
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine("");
+            Console.WriteLine("Firm Balance:" + firmBal.ToString(comma));
+            Between();
             Color(ConsoleColor.Yellow);
             Console.WriteLine("Total Trader Count:" + total);
             Console.WriteLine("Board Member Count:" + board);
             Console.WriteLine("Executive Count:" + exec);
             Console.WriteLine("Associate Count:" + asso);
             Console.WriteLine("Floor Trader Count:" + floor);
-            Color(ConsoleColor.Gray);
-            Console.WriteLine("");
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine("");
+            Between();
             Color(ConsoleColor.Red);
-            Console.WriteLine("Payout Tax Amount:" + tax);
-            Color(ConsoleColor.Gray);
-            Console.WriteLine("");
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine("");
+            Console.WriteLine("Payout Tax Amount:" + tax.ToString(comma));
+            Between();
             Color(ConsoleColor.Green);
-            Console.WriteLine("Total Payout Amount:" + payout);
+            Console.WriteLine("Total Payout Amount:" + _payout);
             Console.WriteLine("Board Member Payout(per member):" + boardPay);
             Console.WriteLine("Executive Payout(per member):" + execPay);
             Console.WriteLine("Associate Payout(per member):" + assoPay);
             Console.WriteLine("Floor Trader Payout(per member):" + floorMemPay);
-            Color(ConsoleColor.Gray);
-            Console.WriteLine("");
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine("");
+            Between();
             Color(ConsoleColor.Red);
             Console.WriteLine("Press a key to start a new search.");
             Console.ReadKey();
@@ -123,18 +107,22 @@ Last Firm Distrubition Update Date:13.03.2019
         }
         static Firm GetFirm(string url)
         {
-            Firm _firm = new Firm();
             HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
-            string jsonData = "";
+            if (request == null) throw new Exception("Unreachable Site");
             using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-            {                                 
-                StreamReader reader = new StreamReader(response.GetResponseStream());
-                jsonData = reader.ReadToEnd();             
+            {
+                if (response == null) throw new Exception("Unreachable Site");
+                else
+                {
+                    StreamReader reader =
+                        new StreamReader(response.GetResponseStream() ?? throw new InvalidOperationException());
+                    var jsonData = reader.ReadToEnd();
+                    var _firm = JsonConvert.DeserializeObject<Firm>(jsonData);
+                    return _firm;
+                }
             }
-            _firm = JsonConvert.DeserializeObject<Firm>(jsonData);
-            return _firm;
         }
-        static void Main(string[] args)
+        static void Main()
         {
             Startup();
 
@@ -147,7 +135,7 @@ Last Firm Distrubition Update Date:13.03.2019
                 if (CheckInt(firmNum))
                 {
                     Firm firm = GetFirm($"https://meme.market/api/firm/{firmNum}");
-                    CalculateOutput(firm.balance, firm.cfo + firm.coo + 1, firm.execs, firm.assocs, (firm.size - firm.cfo - firm.coo - 1 - firm.execs - firm.assocs), firm.name, firm.tax, firm.rank,firm.size);
+                    CalculateOutput(firm.balance, firm.board, firm.execs, firm.assocs, (firm.size - firm.board - 1 - firm.execs - firm.assocs), firm.name, firm.tax, firm.rank,firm.size);
                 }
                 else
                 {
